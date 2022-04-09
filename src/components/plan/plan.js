24dcf009f8c99a8d80ab91a12f673997518c0519
Ilya Timofeev <61859371+Tim-Elias@users.noreply.github.com>
2022-04-10 01:24:23 +0700

    no comment

--- src/components/plan/plan.js
+++ src/components/plan/plan.js
@@ -44,24 +44,11 @@ class GroupMarker extends React.Component {
 
         const colors = this.props.colors
         
-        colors.sort(function (a, b) {
-            if (a.color > b.color) {
-              return 1;
-            }
-            if (a.color < b.color) {
-              return -1;
-            }
-            return 0;
-          });
-
-          const new_colors = colors.map((el,index,arr)=>{
-            
-            let pointStart = arr.filter((c_el,c_index) => c_index < index).length / arr.length * Math.PI * 2
-            let pointEnd = pointStart + (1 / arr.length) * Math.PI * 2
-
-            return {...el, pointStart: pointStart, pointEnd: pointEnd}
-
-          }) 
+        colors.sort((a, b)=>{
+            if (a.color > b.color) {return 1}
+            if (a.color < b.color) {return -1}
+            return 0
+          })
 
         return (
             <div>
@@ -70,13 +57,13 @@ class GroupMarker extends React.Component {
                         {colors.map((el,index) => {
                             return (
                                 <div key={index} style={{ backgroundColor: el.color + "22" }} className='popup_element'>
-                                    {el.type == "block" ? (
+                                    {el.type === "block" ? (
                                         <svg className="popup_element_icon" width="18" height="20" viewBox="0 0 18 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <path fillRule="evenodd" clipRule="evenodd" d="M0.499894 12.7991C0.499777 12.0465 0.500252 11.0632 0.502403 9.99407L0.508058 7.19798C0.510603 5.93993 1.18813 4.78003 2.28271 4.15986L4.76551 2.75316L7.28058 1.32819C8.3509 0.721779 9.66099 0.721792 10.7313 1.32822L15.7264 4.15843C16.8231 4.77984 17.501 5.94303 17.501 7.2036L17.501 12.7964C17.501 14.057 16.8231 15.2202 15.7264 15.8416L13.2464 17.2467L10.731 18.6719C9.66088 19.2783 8.35108 19.2784 7.28086 18.6723L4.7826 17.2575C4.25254 16.9573 3.74443 16.6702 3.28235 16.4096C2.91717 16.2036 2.58077 16.0141 2.28503 15.8477C1.18471 15.2284 0.500092 14.0651 0.499894 12.7991ZM2.5024 9.9981C2.50025 11.0655 2.49978 12.0473 2.49989 12.7987C2.49998 13.339 2.79205 13.838 3.26589 14.1047C3.37322 14.1651 3.4859 14.2285 3.60336 14.2947L12.9908 4.9072L9.74536 3.06832C9.28666 2.80842 8.72519 2.80841 8.26648 3.0683L3.26862 5.89997C2.79952 6.16576 2.50914 6.66286 2.50805 7.20202L2.5024 9.9981ZM14.7953 5.93116L5.4113 15.3152C5.52894 15.3817 5.64799 15.4491 5.76817 15.5172L8.26643 16.932C8.7251 17.1917 9.28644 17.1917 9.74506 16.9318L14.7405 14.1015C15.2105 13.8352 15.501 13.3367 15.501 12.7964L15.501 7.2036C15.501 6.68469 15.233 6.20429 14.7953 5.93116Z" fill={el.color} />
                                         </svg>
                                     ) : (null)}
 
-                                    {el.type == "viev" ? (
+                                    {el.type === "viev" ? (
                                         <svg className="popup_element_icon" width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <path fillRule="evenodd" clipRule="evenodd" d="M15.4778 17.879C15.3396 17.7945 12.0868 14.0087 11.1135 12.8188L11.0971 12.797C10.0057 13.5554 8.67969 14 7.24988 14C3.52195 14 0.499877 10.9779 0.499877 7.25C0.499877 3.52208 3.52195 0.500001 7.24988 0.500001C10.9778 0.500001 13.9999 3.52208 13.9999 7.25C13.9999 8.67618 13.5576 9.99904 12.8027 11.0889L12.8246 11.105C13.9947 12.0679 17.8078 15.3411 17.8878 15.4647C18.0527 15.7193 18.0336 16.2319 17.8439 16.6465C17.3948 17.6275 16.1374 18.2825 15.4778 17.879ZM7.24988 12C9.87323 12 11.9999 9.87335 11.9999 7.25C11.9999 4.62665 9.87323 2.5 7.24988 2.5C4.62652 2.5 2.49988 4.62665 2.49988 7.25C2.49988 9.87335 4.62652 12 7.24988 12ZM7.49988 4C6.94759 4 6.49988 4.44772 6.49988 5C6.49988 5.55229 6.94759 6 7.49988 6C7.9348 6 8.30733 6.27823 8.44424 6.66945C8.62668 7.19074 9.19716 7.46542 9.71844 7.28299C10.2397 7.10055 10.5144 6.53007 10.332 6.00879C9.92306 4.84038 8.8109 4 7.49988 4Z" fill={el.color} />
                                         </svg>) : (null)}
@@ -92,13 +79,15 @@ class GroupMarker extends React.Component {
 
                     <circle className="marker_circle shadow" cx="18.75" cy="18.75" r="16" fill="white" />
 
-                    {new_colors.map(el=>{
+                    {colors.map((el,index,arr)=>{
 
-                        const d = "M " + (Math.sin(el.pointStart)*12.5+18.75) + " " + (Math.cos(el.pointStart)*12.5+18.75) + " a 12.5 12.5 0 0 0 " +(Math.sin(el.pointEnd)-Math.sin(el.pointStart))*12.5 + " " + (Math.cos(el.pointEnd)-Math.cos(el.pointStart))*12.5
-console.log(d)
+                         const pointStart = index / arr.length * Math.PI * 2
+                         const pointEnd = pointStart + (1 / arr.length) * Math.PI * 2
+                        
+                         const d = "M " + (Math.sin(pointStart)*12.5+18.75) + " " + (Math.cos(pointStart)*12.5+18.75) + " a 12.5 12.5 0 0 0 " +(Math.sin(pointEnd)-Math.sin(pointStart))*12.5 + " " + (Math.cos(pointEnd)-Math.cos(pointStart))*12.5
+
                         return (
-                            <path d={d} stroke={el.color} strokeWidth="3" fill="none" />
-
+                            <path key={index} d={d} stroke={el.color} strokeWidth="3" fill="none" />
                         )
                     })}
 
@@ -134,7 +123,7 @@ class Screen extends React.Component {
         let new_arr = this.props.store.test.test_data[this.props.store.test.plan_index].markers.map((el, index, arr) => {
 
             if (arr.filter((cur_el, cur_index) => {
-                return this.calc_distance(cur_el, el) < distance && cur_index != index
+                return this.calc_distance(cur_el, el) < distance && cur_index !== index
             }).length > 0) {
                 return { ...el, hover: false, grouped: true }
             } else {
@@ -168,7 +157,7 @@ class Screen extends React.Component {
             } else {
                 if (arr.filter((cur_el, cur_index) => {
                     return this.calc_distance(cur_el, el) < distance && cur_index < index
-                }).length == 0) {
+                }).length === 0) {
                     group_arr.push(el)
                 }
             }
@@ -186,7 +175,7 @@ class Screen extends React.Component {
                     let cur_dis = Math.pow((Math.pow((element.x - el.x), 2) + Math.pow((element.y - el.y), 2)), 0.5)
                     if (array.filter((cur_el, cur_index) => {
                         return this.calc_distance(cur_el, el) < cur_dis && cur_index !== ind
-                    }).length == 0) {
+                    }).length === 0) {
                         cur_group = element
                     }
                 });
@@ -299,7 +288,7 @@ class Screen extends React.Component {
                    
                     
                 
-                        <img onClick={(e) => { this.props.clear_hidden() }} className='plan_img' style={{ height: this.props.store.test.plan_size.h * this.props.store.test.scale, width: this.props.store.test.plan_size.w * this.props.store.test.scale }} src={planes[this.props.store.test.plan_index]} />
+                        <img onClick={(e) => { this.props.clear_hidden() }} className='plan_img' style={{ height: this.props.store.test.plan_size.h * this.props.store.test.scale, width: this.props.store.test.plan_size.w * this.props.store.test.scale }} src={planes[this.props.store.test.plan_index]} alt="plan"/>
 
                         <div ref={this.scrollTo} style={{ position: 'absolute', top: ((this.props.store.test.data_size.top + this.props.store.test.data_size.bottom) / 2 * this.props.store.test.scale) + 'px', left: ((this.props.store.test.data_size.left + this.props.store.test.data_size.right) / 2 * this.props.store.test.scale) + 'px' }}></div>
 
@@ -311,7 +300,8 @@ class Screen extends React.Component {
                                     key={index} style={{ top: (i.y * this.props.store.test.scale) - 68 + 'px', left: (i.x * this.props.store.test.scale) - 30 + 'px' }}>
                                     <SimpleMarker type={i.type} label={i.label} color={i.color} hover={i.hover} />
                                 </div>
-                             }
+                             } 
+                             return ""
                         })}
 
                         {this.props.store.test.group_arr.map((i, index) => {
